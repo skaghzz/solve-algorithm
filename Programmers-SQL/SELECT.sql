cbb6ebfd8dbebdab51ec668be010252fa0cdaf98
namhokim <skaghzz@naver.com>
2021-08-12 19:13:55 +0900

    select-여러 기준으로 정렬하기

--- Programmers-SQL/SELECT.sql
+++ Programmers-SQL/SELECT.sql
@@ -22,3 +22,8 @@ SELECT ANIMAL_ID, NAME FROM ANIMAL_INS WHERE INTAKE_CONDITION <> "Aged"
 -- https://programmers.co.kr/learn/courses/30/lessons/59403
 SELECT ANIMAL_ID, NAME FROM ANIMAL_INS ORDER BY ANIMAL_ID
 ;
+
+-- 여러 기준으로 정렬하기
+-- https://programmers.co.kr/learn/courses/30/lessons/59404
+SELECT ANIMAL_ID, NAME, DATETIME FROM ANIMAL_INS ORDER BY NAME ASC, DATETIME DESC
+;
