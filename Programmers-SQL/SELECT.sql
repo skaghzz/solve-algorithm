f76eddd56a41451fff99b9686c821b14f682331d
namhokim <skaghzz@naver.com>
2021-08-12 18:59:09 +0900

    select-역순 정렬하기

--- Programmers-SQL/SELECT.sql
+++ Programmers-SQL/SELECT.sql
@@ -2,3 +2,8 @@
 -- https://programmers.co.kr/learn/courses/30/lessons/59034
 SELECT * FROM ANIMAL_INS ORDER BY ANIMAL_ID
 ;
+
+-- 역순 정렬하기
+-- https://programmers.co.kr/learn/courses/30/lessons/59035
+SELECT NAME, DATETIME FROM ANIMAL_INS ORDER BY ANIMAL_ID DESC
+;
