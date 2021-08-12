4dab3947e5aca85def356668c32d5add92ae2900
namhokim <skaghzz@naver.com>
2021-08-12 19:54:40 +0900

    GROUP_BY - 동명 동물 수 찾기

--- Programmers-SQL/GROUP_BY.sql
+++ Programmers-SQL/GROUP_BY.sql
@@ -2,3 +2,8 @@
 -- https://programmers.co.kr/learn/courses/30/lessons/59040
 SELECT ANIMAL_TYPE, COUNT(*) FROM ANIMAL_INS GROUP BY ANIMAL_TYPE ORDER BY ANIMAL_TYPE ASC
 ;
+
+-- 동명 동물 수 찾기
+-- https://programmers.co.kr/learn/courses/30/lessons/59041
+SELECT NAME, COUNT(*) FROM ANIMAL_INS WHERE NAME IS NOT NULL GROUP BY NAME HAVING COUNT(*) >= 2 ORDER BY NAME
+;
