55fac664f5ba098f5d9ebac31fcf0f4b99206049
namhokim <skaghzz@naver.com>
2021-08-12 19:31:17 +0900

    min - 최솟값 구하기

--- Programmers-SQL/SUM_MAX_MIN.sql
+++ Programmers-SQL/SUM_MAX_MIN.sql
@@ -2,3 +2,8 @@
 -- https://programmers.co.kr/learn/courses/30/lessons/59415
 SELECT MAX(DATETIME) FROM ANIMAL_INS
 ;
+
+-- 최솟값 구하기
+-- https://programmers.co.kr/learn/courses/30/lessons/59038
+SELECT MIN(DATETIME) FROM ANIMAL_INS
+;
