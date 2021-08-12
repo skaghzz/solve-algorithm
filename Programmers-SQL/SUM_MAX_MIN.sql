b55d0bad01659d96748f1850d4d306b13f825b94
namhokim <skaghzz@naver.com>
2021-08-12 19:31:47 +0900

    COUNT - 동물 수 구하기

--- Programmers-SQL/SUM_MAX_MIN.sql
+++ Programmers-SQL/SUM_MAX_MIN.sql
@@ -7,3 +7,8 @@ SELECT MAX(DATETIME) FROM ANIMAL_INS
 -- https://programmers.co.kr/learn/courses/30/lessons/59038
 SELECT MIN(DATETIME) FROM ANIMAL_INS
 ;
+
+-- 동물 수 구하기
+-- https://programmers.co.kr/learn/courses/30/lessons/59406
+SELECT COUNT(*) FROM ANIMAL_INS
+;
