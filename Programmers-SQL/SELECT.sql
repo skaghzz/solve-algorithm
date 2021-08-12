0dff366dfef9cd29bc5bab95073a09a4a68a7922
namhokim <skaghzz@naver.com>
2021-08-12 19:04:47 +0900

    select-어린 동물 찾기

--- Programmers-SQL/SELECT.sql
+++ Programmers-SQL/SELECT.sql
@@ -12,3 +12,8 @@ SELECT NAME, DATETIME FROM ANIMAL_INS ORDER BY ANIMAL_ID DESC
 -- https://programmers.co.kr/learn/courses/30/lessons/59036
 SELECT ANIMAL_ID, NAME FROM ANIMAL_INS WHERE INTAKE_CONDITION = "Sick" ORDER BY ANIMAL_ID
 ;
+
+-- 어린 동물 찾기
+-- https://programmers.co.kr/learn/courses/30/lessons/59037
+SELECT ANIMAL_ID, NAME FROM ANIMAL_INS WHERE INTAKE_CONDITION <> "Aged"
+;
