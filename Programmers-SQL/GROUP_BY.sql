e4085fe18777af69eaf9973da7b74f828fb15298
namhokim <skaghzz@naver.com>
2021-08-13 09:38:35 +0900

    intent 정리

--- Programmers-SQL/GROUP_BY.sql
+++ Programmers-SQL/GROUP_BY.sql
@@ -1,28 +1,39 @@
 -- 고양이와 개는 몇 마리 있을까
 -- https://programmers.co.kr/learn/courses/30/lessons/59040
-SELECT ANIMAL_TYPE, COUNT(*) FROM ANIMAL_INS GROUP BY ANIMAL_TYPE ORDER BY ANIMAL_TYPE ASC
-;
+SELECT  ANIMAL_TYPE
+       ,COUNT(*)
+FROM ANIMAL_INS
+GROUP BY  ANIMAL_TYPE
+ORDER BY ANIMAL_TYPE ASC ;
 
 -- 동명 동물 수 찾기
 -- https://programmers.co.kr/learn/courses/30/lessons/59041
-SELECT NAME, COUNT(*) FROM ANIMAL_INS WHERE NAME IS NOT NULL GROUP BY NAME HAVING COUNT(*) >= 2 ORDER BY NAME
-;
+SELECT  NAME
+       ,COUNT(*)
+FROM ANIMAL_INS
+WHERE NAME IS NOT NULL
+GROUP BY  NAME
+HAVING COUNT(*) >= 2
+ORDER BY NAME ;
 
--- 입양 시각 구하기(1)
+-- 입양 시각 구하기（1）
 -- https://programmers.co.kr/learn/courses/30/lessons/59412
-SELECT hour(DATETIME) as HOUR, COUNT(*) AS COUNT FROM ANIMAL_OUTS GROUP BY HOUR HAVING HOUR BETWEEN 9 AND 20 ORDER BY HOUR
-;
+SELECT  hour(DATETIME) AS HOUR
+       ,COUNT(*)       AS COUNT
+FROM ANIMAL_OUTS
+GROUP BY  HOUR
+HAVING HOUR BETWEEN 9 AND 20
+ORDER BY HOUR ;
 
--- 입양 시각 구하기(2)
+-- 입양 시각 구하기（2）
 -- https://programmers.co.kr/learn/courses/30/lessons/59413
-set @time=-1;
+SET @time=-1;
 
-select @time:=@time+1,
-  (
-      select count(*)
-        from ANIMAL_OUTS
-       where hour(datetime)=@time
-  )
-from ANIMAL_OUTS
-where @time<23
-order by @time
+SELECT  @time:=@time+1
+       ,(
+SELECT  COUNT(*)
+FROM ANIMAL_OUTS
+WHERE hour(datetime)=@time )
+FROM ANIMAL_OUTS
+WHERE @time<23
+ORDER BY @time
--- Programmers-SQL/SUM_MAX_MIN.sql
+++ Programmers-SQL/SUM_MAX_MIN.sql
@@ -1,19 +1,19 @@
 -- 최댓값 구하기
 -- https://programmers.co.kr/learn/courses/30/lessons/59415
-SELECT MAX(DATETIME) FROM ANIMAL_INS
-;
+SELECT  MAX(DATETIME)
+FROM ANIMAL_INS ;
 
 -- 최솟값 구하기
 -- https://programmers.co.kr/learn/courses/30/lessons/59038
-SELECT MIN(DATETIME) FROM ANIMAL_INS
-;
+SELECT  MIN(DATETIME)
+FROM ANIMAL_INS ;
 
 -- 동물 수 구하기
 -- https://programmers.co.kr/learn/courses/30/lessons/59406
-SELECT COUNT(*) FROM ANIMAL_INS
-;
+SELECT  COUNT(*)
+FROM ANIMAL_INS ;
 
 -- 중복 제거하기
 -- https://programmers.co.kr/learn/courses/30/lessons/59408
-SELECT COUNT(DISTINCT NAME) FROM ANIMAL_INS
-;
+SELECT  COUNT(DISTINCT NAME)
+FROM ANIMAL_INS ;
