e67e82a9eddef1df4ee8cc74a6223208cf30a8a8
namho kim <skaghzz@naver.com>
2021-08-15 22:39:26 +0900

    백준 제출용 템플릿

---
+++ BOJ-Template/Main.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        // 입력 값 셋팅
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        String input = br.readLine();
+        int answer = 0;
+        System.out.println(answer);
+    }
+}
